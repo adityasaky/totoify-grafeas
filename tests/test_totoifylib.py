fbddc987121ef3a06998ffdc0cf6f6865bc0fa82
Aditya Sirish <aditya@saky.in>
2020-09-15 10:22:15 -0400

    Rename module
    Signed-off-by: Aditya Sirish <aditya@saky.in>

--- tests/test_totoifylib.py
+++ tests/test_totoifylib.py
@@ -1,5 +1,5 @@
 import unittest
-from totoify_grafeas.totoify_grafeas import (
+from totoify_grafeas.totoifylib import (
     create_grafeas_occurrence_from_in_toto_link,
     create_in_toto_link_from_grafeas_occurrence,
     GrafeasInTotoTransport,
