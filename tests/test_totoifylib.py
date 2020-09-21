6fe6ee83c8e038440ceaac6d31d89b06b16ef623
Kristel Fung <kristelfung@gmail.com>
2020-09-21 18:25:44 +0800

    load occurence and note json files

--- tests/test_totoifylib.py
+++ tests/test_totoifylib.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from totoify_grafeas.totoifylib import (
     create_grafeas_occurrence_from_in_toto_link,
     create_in_toto_link_from_grafeas_occurrence,
@@ -8,5 +9,12 @@ from totoify_grafeas.totoifylib import (
 
 
 class TestCreateOccurrenceFromInTotoLink(unittest.TestCase):
-  def test_something():
-    pass
+  def test_intoto_verify_link_from_occurence(self):
+    with open("occurrence_link.json", "r") as occ_f:
+      occurence_json = json.load(occ_f)
+    with open("note_reduced.json", "r") as note_f:
+      note_json = json.load(note_f)
+    
+    
+if __name__ == "__main__":
+  unittest.main()
