8b72503e405a410a33a992b4fb535c84e043804f
Kristel Fung <kristelfung@gmail.com>
2020-09-23 22:13:16 +0800

    Update test_totoifylib.py

--- tests/test_totoifylib.py
+++ tests/test_totoifylib.py
@@ -16,11 +16,13 @@ class TestCreateInTotoLinkFromOccurrence(unittest.TestCase):
     with open("note_reduced.json", "r") as note_f:
       note_json = json.load(note_f)
 
-    occ = GrafeasInTotoOccurrence(occurrence_json["intoto"],
-        note_json["intoto"]["step_name"],
-        occurrence_json["intoto"]["signed"]["products"][0]["resource_uri"])
-
-    
+    occ = create_in_toto_link_from_grafeas_occurrence(occurrence_json, "clone")
+#    occ = GrafeasInTotoOccurrence(occurrence_json["intoto"],
+#        note_json["intoto"]["step_name"],
+#        occurrence_json["intoto"]["signed"]["products"][0]["resource_uri"])
+#
+#    link = create_in_toto_link_from_grafeas_occurrence(occ, note_json["intoto"]["step_name"])
+    # run in-toto verify on this link - do I have to import intoto?
 
 
 if __name__ == "__main__":
